746d6297302c070086d58e2f79e2ef385331aa25
Hong Quach <htquach@cecs.pdx.edu>
2011-01-26 23:05:49 -0800

    Added import for hashlib, smtplib, and ActivateLoginForm. Factored "gen = Generator()" to the top of each function to avoid repeat code. login():  Added logic to login() to properly handle user input and check for user.activestatus; and modified the logic to display single error message when user failed to login (either invalid username or password). singup():  Added code to support user.activestatus and work around email feature to display on the signup.html page. activatelogin():  New function to support account activation. send_activationcode():  New function to support account activation;  This feature use smtplib to send mail but require SMTP server infomation to go live;  The work around is to return a tuple including email body. get_activationcode():  New function to support account activation.

--- cockerel/webapp/admin/admin.py
+++ cockerel/webapp/admin/admin.py
@@ -1,3 +1,6 @@
+import hashlib
+import smtplib
+
 from flask import (
     g,
     Module,
@@ -9,8 +12,10 @@ from flask import (
 )
 from flatland.out.markup import Generator
 
+#TODO: import cockerel.auth.security does not work.
+#from cockerel.auth.security import get_activationcode
 from cockerel.models.schema import db, User
-from cockerel.forms import LoginForm, SignupForm
+from cockerel.forms import LoginForm, SignupForm, ActivateLoginForm
 
 admin = Module(__name__)
 
@@ -19,30 +24,31 @@ admin = Module(__name__)
 def login():
     # TODO: make this do better auth, it needs to set a cookie for a period of
     #  time
+    gen = Generator()
     if request.method == 'POST':
         form = LoginForm.from_flat(request.form)
         if form.validate():
             user = User.query.filter_by(
                 username=request.form['username']).first()
-
-            if user == None:
-                form['username'].add_error(
-                    'Username %s not found' % form['username'].value)
-                gen = Generator()
-                return render_template("admin/login.html", form=form, html=gen)
-
-            if user.check_password(request.form['password']):
-                g.user = user
-                set_user()
-                if request.args:
-                    return redirect(request.args.get('next'))
-                else:
-                    return redirect(url_for('frontend.index'))
+            if user != None:
+                if not user.activestatus:
+                    form.add_error(
+                        """Username %s need to be activated before 1st login.
+                        It can only be activated with the link sent to email of this username.""" %
+                        form['username'].value)
+                    return render_template("admin/login.html", form=form, html=gen)
+                if user.check_password(request.form['password']):
+                    g.user = user
+                    set_user()
+                    if request.args:
+                        return redirect(request.args.get('next'))
+                    else:
+                        return redirect(url_for('frontend.index'))
+            form.add_error('Invalid username %s or password.' % form['username'].value)
+            return render_template("admin/login.html", form=form, html=gen)
         else:
-            gen = Generator()
             return render_template("admin/login.html", form=form, html=gen)
     form = LoginForm()
-    gen = Generator()
     return render_template("admin/login.html",
                            form=form,
                            html=gen,
@@ -57,6 +63,7 @@ def logout():
 
 @admin.route('/signup', methods=['GET', 'POST'])
 def signup():
+    gen = Generator()
     if request.method == 'POST':
         form = SignupForm.from_flat(request.form)
         if form.validate():
@@ -78,24 +85,94 @@ def signup():
                        request.form['password'],
                        request.form['email'],
                        request.form['firstname'],
-                       request.form['lastname'])
-    
+                       request.form['lastname'],
+                       False)
             db.session.add(user)
             db.session.commit()
-            g.user = user
-            set_user()
-            return redirect(url_for('frontend.index'))
+            #TODO work around since SMTP mail server is not setup.
+            form = SignupForm()
+            form.add_error(send_activationcode(user))
+            return render_template("admin/signup.html",
+                                   form=form,
+                                   html=gen)
         else:
-            gen = Generator()
             return render_template("admin/signup.html", 
                                    form=form, 
                                    html=gen)
     form = SignupForm()
-    gen = Generator()
     return render_template("admin/signup.html",
                            form=form,
                            html=gen)
 
+@admin.route('/activatelogin', methods=['GET', 'POST'])
+def activatelogin():
+    gen = Generator()
+    if request.method == 'POST':
+        form = ActivateLoginForm.from_flat(request.form)
+        if form.validate():
+            user = User.query.filter_by(
+                username=request.form['username']).first()
+            if user == None:
+                form['username'].add_error(
+                    'Username %s is invalid' % form['username'].value)
+                return render_template("admin/activatelogin.html", form=form, html=gen)
+            if get_activationcode(user) != request.args['activationcode']:
+                form.add_error(
+                    'incorrect user name or invalid activation code %s.' %
+                    request.args['activationcode'])
+                return render_template("admin/activatelogin.html",
+                                       form=form,
+                                       html=gen)
+            user.activestatus = True
+            db.session.commit()
+            form = LoginForm()
+            return render_template("admin/login.html",
+                                   form=form,
+                                   html=gen)
+        else:
+            return render_template("admin/activatelogin.html",
+                                   form=form,
+                                   html=gen)
+    form = ActivateLoginForm()
+    return render_template("admin/activatelogin.html",
+                           form=form,
+                           html=gen)
+
+def send_activationcode(user):
+    sender = 'Cockerel@Cockerel.com'
+    receivers = user.email
+    activationURL = "http://127.0.0.1:5000" +\
+                    url_for('activatelogin',
+                            activationcode=get_activationcode(user))
+    message = """From: Cockerel <%s>
+    To: %s %s <%s>
+    Subject: Welcome to Cockerel
+
+    Hi %s,
+
+    Your username is %s.  Please use this link to activate your account %s.
+
+    Thank you,
+    Cockerel
+    """ % (sender,
+           user.firstname,
+           user.lastname,
+           receivers,
+           user.firstname,
+           user.username,
+           activationURL)
+    return sender, receivers, message
+#    TODO: Supply SMTP server information to use the email feature
+#    try:
+#       smtpObj = smtplib.SMTP('localhost')
+#       smtpObj.sendmail(sender, receivers, message)
+#       return True
+#    except smtplib.SMTPException:
+#       return False
+
+def get_activationcode(user):
+    return hashlib.sha1(user.username.lower() +
+                        user.email.lower()).hexdigest()
 
 def check_user():
     g.user = User.query.filter_by(
